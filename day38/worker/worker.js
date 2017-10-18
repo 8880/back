a5e0708613a42bb591c0d03fae9c705df9bf36e7
klous <klous530.outlook.com>
2017-10-18 21:04:32 +0800

    new

---
+++ day38/worker/worker.js
@@ -0,0 +1,11 @@
+
+var timer, count = 0 ;
+timer = setInterval(function(){
+  console.log(count++);
+  this.postMessage(count);
+
+  if (count >= 10){
+    throw new Error("到达极限");
+  }
+
+}, 1000);
